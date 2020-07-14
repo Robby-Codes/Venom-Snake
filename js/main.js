ba9732ffe7d731cd008603ccaac0ed996991e2a5
Roberto Guerra <contact@robertoguerra.info>
2020-07-14 13:40:18 -0400

    Added some javascript for interactivity. Although this could have been done with css, I wanted to practice using javascript.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 const cover = document.getElementById("cover");
+const aboutButton = document.getElementById("about-btn");
 
-cover.onload = moveCover;
+onload = moveCover;
+aboutButton.addEventListener("mouseover", buttonAnimation1);
+aboutButton.addEventListener("mouseout", buttonAnimation2);
 
-function moveCover() {
-    alert('this ran');
+//This is going to be useful later on.
+//skillLevel.scrollIntoView({behavior: 'smooth', block: 'center'});
+
+function moveCover () {
+    cover.style.transition = "all 2s 2s";
+    cover.style.transform = "translate(-100%, 0)";
+}
+
+function buttonAnimation1 () {
+    aboutButton.style.transition = "all 0.5s";
+    aboutButton.style.color = "#26262a";
+    aboutButton.style.backgroundColor = "#dc626a";
+}
+
+function buttonAnimation2 () {
+    aboutButton.style.transition = "all 0.5s";
+    aboutButton.style.color = "#dc626a";
+    aboutButton.style.backgroundColor = "transparent";
 }
