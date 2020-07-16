d50a76b6af7d5f8f96911f183cacc98389fe1535
Roberto Guerra <contact@robertoguerra.info>
2020-07-15 22:36:42 -0400

    Added some functions that were missing.

--- js/main.js
+++ js/main.js
@@ -48,6 +48,23 @@ function partnerSmoothScroll () {
     document.getElementById("partners-section").scrollIntoView({behavior: 'smooth', block: 'center'});
 }
 
+function moveCover () {
+    cover.style.transition = "all 2s 1s";
+    cover.style.transform = "translate(-100%, 0)";
+}
+
+function buttonAnimation1 () {
+    aboutButton.style.transition = "all 0.5s";
+    aboutButton.style.color = "#26262a";
+    aboutButton.style.backgroundColor = "#dc626a";
+}
+
+function buttonAnimation2 () {
+    aboutButton.style.transition = "all 0.5s";
+    aboutButton.style.color = "#dc626a";
+    aboutButton.style.backgroundColor = "transparent";
+}
+
 function vsStatsIntoView (element) {
     var windowHeight = window.innerHeight;
     window.addEventListener('scroll', () => {
