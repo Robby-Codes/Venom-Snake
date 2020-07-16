5776b852519d3c6f5c2abf5f5239745b60a510b8
Roberto Guerra <contact@robertoguerra.info>
2020-07-15 22:32:33 -0400

    Added more JavaScript to add more characteristics to the page such as smooth scrolling upon clicking a navigation link, skill bars that move into place when scrolled into view, and a welcome cover page that appears when the user first visits the page.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,95 @@
 const cover = document.getElementById("cover");
 const aboutButton = document.getElementById("about-btn");
 
+const combatLevel = document.getElementsByClassName('combat-level');
+const rdLevel = document.getElementsByClassName('rd-level');
+const bdLevel = document.getElementsByClassName('bd-level');
+const supportLevel = document.getElementsByClassName('support-level');
+const intelLevel = document.getElementsByClassName('intel-level');
+const medicalLevel = document.getElementsByClassName('medical-level');
+
+const qCombatLevel = document.getElementsByClassName('q-combat-level');
+const qrdLevel = document.getElementsByClassName('q-rd-level');
+const qbdLevel = document.getElementsByClassName('q-bd-level');
+const qSupportLevel = document.getElementsByClassName('q-support-level');
+const qIntelLevel = document.getElementsByClassName('q-intel-level');
+const qMedicalLevel = document.getElementsByClassName('q-medical-level');
+
+
 onload = moveCover;
 aboutButton.addEventListener("mouseover", buttonAnimation1);
 aboutButton.addEventListener("mouseout", buttonAnimation2);
 
-//This is going to be useful later on.
-//skillLevel.scrollIntoView({behavior: 'smooth', block: 'center'});
+vsStatsIntoView(combatLevel);
+vsStatsIntoView(rdLevel);
+vsStatsIntoView(bdLevel);
+vsStatsIntoView(supportLevel);
+vsStatsIntoView(intelLevel);
+vsStatsIntoView(medicalLevel);
 
-function moveCover () {
-    cover.style.transition = "all 2s 2s";
-    cover.style.transform = "translate(-100%, 0)";
+qStatsIntoView(qCombatLevel);
+qStatsIntoView(qrdLevel);
+qStatsIntoView(qbdLevel);
+qStatsIntoView(qSupportLevel);
+qStatsIntoView(qIntelLevel);
+qStatsIntoView(qMedicalLevel);
+
+document.getElementsByClassName("about-link")[0].addEventListener('click', aboutSmoothScroll);;
+document.getElementsByClassName("skills-link")[0].addEventListener('click', skillsSmoothScroll);;
+document.getElementsByClassName("partner-link")[0].addEventListener('click', partnerSmoothScroll);;
+
+function aboutSmoothScroll () {
+    document.getElementById("about-section").scrollIntoView({behavior: 'smooth', block: 'center'});
+}
+function skillsSmoothScroll () {
+    document.getElementById("skills-section").scrollIntoView({behavior: 'smooth', block: 'center'});
+}
+function partnerSmoothScroll () {
+    document.getElementById("partners-section").scrollIntoView({behavior: 'smooth', block: 'center'});
 }
 
-function buttonAnimation1 () {
-    aboutButton.style.transition = "all 0.5s";
-    aboutButton.style.color = "#26262a";
-    aboutButton.style.backgroundColor = "#dc626a";
+function vsStatsIntoView (element) {
+    var windowHeight = window.innerHeight;
+    window.addEventListener('scroll', () => {
+        for (var x = 0; x < element.length; i++) {
+            var elem = element[x];
+            var position = element[x].getBoundingClientRect().top;
+            if (position - windowHeight <= 0) {
+                elem.style.transition = "all 2s";
+                elem.style.width = "70%";
+            }
+        }
+    });
 }
 
-function buttonAnimation2 () {
-    aboutButton.style.transition = "all 0.5s";
-    aboutButton.style.color = "#dc626a";
-    aboutButton.style.backgroundColor = "transparent";
+function qStatsIntoView (element) {
+    var windowHeight = window.innerHeight;
+    window.addEventListener('scroll', () => {
+        for (var x = 0; x < element.length; i++) {
+            var elem = element[x];
+            var position = element[x].getBoundingClientRect().top;
+            if (position - windowHeight <= 0) {
+                elem.style.transition = "all 2s";
+                switch (elem) {
+                    case document.getElementsByClassName('q-combat-level')[0]:
+                    case document.getElementsByClassName('q-intel-level')[0]:
+                        elem.style.width = "70%";
+                        break;
+                    case document.getElementsByClassName('q-rd-level')[0]:
+                    case document.getElementsByClassName('q-medical-level')[0]:
+                        elem.style.width = "20%";
+                        break;
+                    case document.getElementsByClassName('q-bd-level')[0]:
+                        elem.style.width = "10%";
+                        break;
+                    case document.getElementsByClassName('q-support-level')[0]:
+                        elem.style.width = "30%";
+                        break;
+                    default:
+                        alert('something went wrong');
+                        break;
+                }
+            }
+        }
+    });
 }
