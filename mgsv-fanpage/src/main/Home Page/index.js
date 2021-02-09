b047a4691074babe1f56a929b2088f46c1d4fa3c
Robby-Codes <contact@robertoguerra.info>
2021-02-09 18:37:46 -0500

    Home Page inspired by mgsv's aerial command center

--- mgsv-fanpage/src/main/Home Page/index.js
+++ mgsv-fanpage/src/main/Home Page/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home.css";
-import homebg from "../../assets/homebg.jpg";
+import homevideoloop from "../../assets/hploop.mp4";
 
 const HomePage = () => {
   return <Menu />;
@@ -9,17 +9,24 @@ const HomePage = () => {
 const Menu = () => {
   return (
     <div className="homepage-container">
+      <div className="video-container">
+        <video autoPlay loop>
+          <source src={homevideoloop} type="video/mp4" />
+          Sorry, Your Browser Cannot Play This Video
+        </video>
+      </div>
       <div className="text-container">
-        <h1>Metal Gear Solid V</h1>
+        <h1>METAL GEAR SOLID V</h1>
         <h1>THE PHANTOM PAIN</h1>
         <h1>THE UNOFFICIAL FANPAGE</h1>
         <a id="vs-link" href=".">
-          <h1>Venom Snakes Profile</h1>
+          <h1>VENOM SNAKE PROFILE</h1>
         </a>
         <a id="dd-link" href=".">
-          <h1>About Diamond Dog</h1>
+          <h1>ABOUT DIAMOND DOGS</h1>
         </a>
       </div>
+      <small>Created By: Roberto Guerra</small>
     </div>
   );
 };
